38a70a2e6f43af03e03eedd83851d7c30f083909
isabellaim <iimartin@espol.edu.ec>
2024-08-17 18:13:32 -0500

    cambios en interface lista

---
+++ src/main/java/ec/edu/espol/proyectoed/p/controller/PlantillaAnimalController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.proyectoed.p.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author isabella
+ */
+public class PlantillaAnimalController implements Initializable {
+
+    @FXML
+    private VBox vbAni1;
+    @FXML
+    private Text txtAni1;
+    @FXML
+    private ImageView ivAni1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
