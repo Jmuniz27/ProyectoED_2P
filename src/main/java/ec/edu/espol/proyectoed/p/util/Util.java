18c2544b25f562f4d53681fdad075633cb9ee034
Juan Munizaga Torres <juanmuni@espol.edu.ec>
2024-08-22 22:08:28 -0500

    eliminacion metodos innecesarios

--- src/main/java/ec/edu/espol/proyectoed/p/util/Util.java
+++ src/main/java/ec/edu/espol/proyectoed/p/util/Util.java
@@ -1,48 +1,8 @@
 package ec.edu.espol.proyectoed.p.util;
 
-import ec.edu.espol.proyectoed.p.modelo.BinaryTree;
-import ec.edu.espol.proyectoed.p.modelo.NodeBinaryTree;
-import javafx.scene.Node;
-
-import java.util.List;
-import java.util.Map;
 import javafx.scene.control.Alert;
 
 public class Util {
-    
-    
-    public static BinaryTree<String> buildDecisionTree(List<String> questions, Map<String, List<String>> animalsWithAnswers) {
-        BinaryTree<String> tree = new BinaryTree<>();
-        for (Map.Entry<String, List<String>> entry : animalsWithAnswers.entrySet()) {
-            tree.setRoot(addAnimal(tree.getRoot(), questions, entry.getKey(), entry.getValue(), 0));
-        }
-        return tree;
-    }
-
-    private static NodeBinaryTree<String> addAnimal(NodeBinaryTree node, List<String> questions, String animal, List<String> answers, int index) {
-        if (index == questions.size()) {
-            return new NodeBinaryTree<>(animal);
-        }
-
-        if (node == null) {
-            node = new NodeBinaryTree<>(questions.get(index));
-        }
-
-        if (answers.get(index).equalsIgnoreCase("yes")) {
-            if (node.getLeft() == null) {
-                node.setLeft(new BinaryTree<String>());
-            }
-            node.getLeft().setRoot(addAnimal(node.getLeft().getRoot(), questions, animal, answers, index + 1));
-        } else {
-            if (node.getRight() == null) {
-                node.setRight(new BinaryTree<>());
-            }
-            node.getRight().setRoot(addAnimal(node.getRight().getRoot(), questions, animal, answers, index + 1));
-        }
-
-        return node;
-    }
-    
     public static void mostrarAlerta(String titulo, String mensaje) {
         Alert alerta = new Alert(Alert.AlertType.ERROR);
         alerta.setTitle(titulo);
